70fe538b074c23ccbab94be2b835fe53ccfea899
Efrem <efremyohanis116@gmail.com>
2025-11-16 07:21:10 -0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,35 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
-
-import { MainLayout } from "@/components/layout/main-layout";
-import { LoginPage } from "@/pages/LoginPage";
-import { DashboardPage } from "@/pages/DashboardPage";
-import { StreamsPage } from "@/pages/StreamsPage";
-import { AlertsPage } from "@/pages/AlertsPage";
-import { ReportsPage } from "@/pages/ReportsPage";
-import { FlowDetailPage } from "@/pages/flows/flow-detail/FlowDetailPage";
-import { FlowEditorRoute } from '@/pages/flows/flow-editor/FlowEditorRoute';
-import { CreateNodePage } from "@/pages/nodes/create-node/CreateNodePage";
-import { NodeDetailPage } from "@/pages/nodes/node-detail/NodeDetailPage";
-import { EditNodePage } from "@/pages/nodes/edit-node/EditNodePage";
-import { TestNodePage } from "@/pages/nodes/test-node/TestNodePage";
-import { SubnodeDetailPage } from "@/pages/subnodes/subnode-detail/SubnodeDetailPage";
-import { EditSubnodePage } from "@/pages/subnodes/edit-subnode/EditSubnodePage";
-import { EditVersionPage } from "@/pages/subnodes/edit-version/EditVersionPage";
-import { EditVersionPage as EditNodeVersionPage } from "@/pages/nodes/edit-version/EditVersionPage";
-import { CreateSubnodePage } from "@/pages/subnodes/create-subnode/CreateSubnodePage";
-import { ParameterDetailPage } from "@/pages/parameters/parameter-detail/ParameterDetailPage";
-import { CreateParameterPage } from "@/pages/parameters/create-parameter/CreateParameterPage";
-import { EditParameterPage } from "@/pages/parameters/edit-parameter/EditParameterPage";
-import { DevToolPage } from "@/pages/DevToolPage";
-import { StreamDetailPage } from "@/pages/StreamDetailPage";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <Navigation />
         <Routes>
-          {/* Login route without layout */}
-          <Route path="/login" element={<LoginPage />} />
-          
-          {/* All other routes with main layout */}
-          <Route path="/*" element={
-            <MainLayout>
-              <Routes>
-                <Route path="/" element={<DashboardPage />} />
-                <Route path="/streams" element={<StreamsPage />} />
-                <Route path="/streams/:streamId" element={<StreamDetailPage />} />
-                <Route path="/alerts" element={<AlertsPage />} />
-                <Route path="/reports" element={<ReportsPage />} />
-                <Route path="/devtool" element={<DevToolPage />} />
-                
-                {/* Flow routes */}
-                <Route path="/flows/:id" element={<FlowDetailPage />} />
-                <Route path="/flows/:id/edit" element={<FlowEditorRoute />} />
-                
-                {/* Node routes */}
-                <Route path="/nodes/new" element={<CreateNodePage />} />
-                <Route path="/nodes/:id" element={<NodeDetailPage />} />
-                <Route path="/nodes/:id/edit" element={<EditNodePage />} />
-                <Route path="/nodes/:id/edit-version" element={<EditNodeVersionPage />} />
-                <Route path="/nodes/:id/test" element={<TestNodePage />} />
-                
-                {/* Subnode routes */}
-                <Route path="/subnodes/:id" element={<SubnodeDetailPage />} />
-                <Route path="/subnodes/create" element={<CreateSubnodePage />} />
-                <Route path="/subnodes/:id/edit" element={<EditSubnodePage />} />
-                <Route path="/subnodes/:id/edit-version" element={<EditVersionPage />} />
-                
-                {/* Parameter routes */}
-                <Route path="/parameters/new" element={<CreateParameterPage />} />
-                <Route path="/parameters/:id" element={<ParameterDetailPage />} />
-                <Route path="/parameters/:id/edit" element={<EditParameterPage />} />
-                
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </MainLayout>
-          } />
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-    </ThemeProvider>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
